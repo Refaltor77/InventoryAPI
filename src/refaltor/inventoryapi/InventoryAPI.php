b71e473e5e39fd97d5b7682c1303c74f664b694b
Refaltor <77900181+Refaltor77@users.noreply.github.com>
2022-03-10 19:01:32 +0100

    Update InventoryAPI.php

--- src/refaltor/inventoryapi/InventoryAPI.php
+++ src/refaltor/inventoryapi/InventoryAPI.php
@@ -25,25 +25,6 @@ class InventoryAPI extends PluginBase
 
     public function onEnable(): void
     {
-        $doubleChestInventory = InventoryAPI::createDoubleChest(isViewOnly: false);
-        $doubleChestInventory->setName('DoubleChestInventory');
-        $doubleChestInventory->setContents([
-            VanillaItems::GOLD_INGOT(),
-            VanillaItems::STEAK()
-        ]);
-        $doubleChestInventory->setClickListener(function (Player $player, BaseInventoryCustom $inventory, Item $sourceItem, Item $targetItem, int $slot): void {
-            if ($slot === 0) {
-                $inventory->transactionCancel();
-            } else {
-                $inventory->addItem(VanillaItems::COAL()->setCustomName("Example"));
-            }
-        });
-        $doubleChestInventory->setCloseListener(function (Player $player, BaseInventoryCustom $inventory): void {
-            Server::getInstance()->broadcastMessage("Hello close chest !");
-        });
-        $doubleChestInventory->send($player);
-
-
         $this->saveDefaultConfig();
         self::$instance = $this;
         $this->getServer()->getPluginManager()->registerEvents(new PacketListener(), $this);
