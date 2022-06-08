56d99709896f4be4db1b9168fedceb6536fa1361
Refaltor <77900181+Refaltor77@users.noreply.github.com>
2022-06-08 19:43:19 +0200

    Update SimpleChestInventory.php

--- src/refaltor/inventoryapi/inventories/SimpleChestInventory.php
+++ src/refaltor/inventoryapi/inventories/SimpleChestInventory.php
@@ -15,12 +15,26 @@ use pocketmine\world\Position;
 
 class SimpleChestInventory extends BaseInventoryCustom
 {
-    public function send(Player $player){
-        $this->holder = new Position((int)$player->getPosition()->getX(), (int)$player->getPosition()->getY() + 3, (int)$player->getPosition()->getZ(), $player->getWorld());
-        $player->getNetworkSession()->sendDataPacket(UpdateBlockPacket::create(BlockPosition::fromVector3($this->holder), RuntimeBlockMapping::getInstance()->toRuntimeId(VanillaBlocks::CHEST()->getFullId()), UpdateBlockPacket::FLAG_NETWORK, UpdateBlockPacket::DATA_LAYER_NORMAL));
-        $nbt = CompoundTag::create()->setString(Nameable::TAG_CUSTOM_NAME, $this->getName());
-        $packet = BlockActorDataPacket::create(BlockPosition::fromVector3($this->holder), new CacheableNbt($nbt));
-        $player->getNetworkSession()->sendDataPacket($packet);
-        $player->setCurrentWindow($this);
+    private array $hasSend = [];
+
+    public function onClose(Player $who): void
+    {
+        if (isset($this->hasSend[$who->getXuid()])) {
+            unset($this->hasSend[$who->getXuid()]);
+        }
+        parent::onClose($who);
+    }
+
+    public function send(Player $player)
+    {
+        if (!isset($this->hasSend[$player->getXuid()])) {
+            $this->holder = new Position((int)$player->getPosition()->getX(), (int)$player->getPosition()->getY() + 3, (int)$player->getPosition()->getZ(), $player->getWorld());
+            $player->getNetworkSession()->sendDataPacket(UpdateBlockPacket::create(BlockPosition::fromVector3($this->holder), RuntimeBlockMapping::getInstance()->toRuntimeId(VanillaBlocks::CHEST()->getFullId()), UpdateBlockPacket::FLAG_NETWORK, UpdateBlockPacket::DATA_LAYER_NORMAL));
+            $nbt = CompoundTag::create()->setString(Nameable::TAG_CUSTOM_NAME, $this->getName());
+            $packet = BlockActorDataPacket::create(BlockPosition::fromVector3($this->holder), new CacheableNbt($nbt));
+            $player->getNetworkSession()->sendDataPacket($packet);
+            $player->setCurrentWindow($this);
+            $this->hasSend[$player->getXuid()] = true;
+        }
     }
 }
